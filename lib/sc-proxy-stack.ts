71008efa094e5d75763880d5699e50a3d7cd3378
Jonathan Williams <tamlin@mac.com>
2023-04-23 20:30:17 -0400

    add an optional certificate
    from the environment.  If there is a certificate ARN (and ID) supplied in the environment, the stack will create an https listener with the specified cert.
    
    Also shrinks the default memory on the task.

--- lib/sc-proxy-stack.ts
+++ lib/sc-proxy-stack.ts
@@ -3,7 +3,16 @@ import { Construct } from 'constructs';
 import { aws_ec2 as ec2 } from 'aws-cdk-lib';
 import { aws_ecs as ecs } from 'aws-cdk-lib';
 import { aws_ecs_patterns as ecs_patterns } from 'aws-cdk-lib';
+import { aws_elasticloadbalancingv2 as elbv2 } from 'aws-cdk-lib';
+import { aws_certificatemanager as acm } from 'aws-cdk-lib';
+
+import { config } from 'dotenv';
+config();
+
 import path = require('path');
+
+// Optional ACM certificate paramters, if a cert is specified it will be used to create an HTTPS listener.
+const { CERT_ID: certID = '', CERT_ARN: certArn = '' } = process.env;
 
 export class ScProxyStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
@@ -27,7 +36,10 @@ export class ScProxyStack extends Stack {
           "name": "Serverless Fargate",
         },
       },
-      memoryLimitMiB: 1024,
+      ...certArn && {protocol: elbv2.ApplicationProtocol.HTTPS},
+      ...certArn && {redirectHTTP: true},
+      ...certArn && {certificate: acm.Certificate.fromCertificateArn(this, certID, certArn)},
+      memoryLimitMiB: 512,
     });
   }
 }
